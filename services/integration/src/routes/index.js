b38ecdd28928cd26544a560670fcc54410883381
RicardoJ <rjdiazzh@gmail.com>
2019-03-18 17:37:12 -0500

    [INTG][OBJECTIVE] - adding controller , route , model of objective , with change of branch feature-constitution

--- services/integration/src/routes/index.js
+++ services/integration/src/routes/index.js
@@ -1,8 +1,12 @@
 const Router = require("express").Router();
 const ProjectRouter = require("./projects");
 const ConstitutionRouter = require("./constitution/constitution");
+const ObjectiveRouter = require("./constitution/objective");
+
 
 Router.use('/projects', ProjectRouter);
+
 Router.use('/constitutions' ,ConstitutionRouter);
+Router.use('/objectives' ,ObjectiveRouter);
 
 module.exports = Router;
