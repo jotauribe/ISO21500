0d20530ef09c40c18ea948d83b2a3f1ee8b1b37b
RicardoJ <rjdiazzh@gmail.com>
2019-03-17 19:03:16 -0500

    [INTG][CONSTITUTION] - change in route in file consitution of routes

--- services/integration/src/routes/constitution/constitution.js
+++ services/integration/src/routes/constitution/constitution.js
@@ -1,10 +1,10 @@
-const Router = require('express').Router();
-const constitution = require('../../controllers/constitution.controllers/constitution');
+const Router = require("express").Router();
+const constitution = require("../../controllers/constitution/constitution");
 
-Router.post('/', constitution.create);
-Router.get('/:id', constitution.get);
-Router.get('/', constitution.find);
-Router.put('/:id', constitution.update);
-Router.delete('/:id', constitution.remove);
+Router.post("/", constitution.create);
+Router.get("/:id", constitution.get);
+Router.get("/", constitution.find);
+Router.put("/:id", constitution.update);
+Router.delete("/:id", constitution.remove);
 
 module.exports = Router;
