a7e33f144229716bb012262ddcff18c2c0906ac1
RicardoJ <rjdiazzh@gmail.com>
2019-03-18 17:54:41 -0500

    [INTG][PHASES] - ADD ROUTE , CONTROLLERS AND MODEL OF PHASES

--- services/integration/src/routes/index.js
+++ services/integration/src/routes/index.js
@@ -2,11 +2,13 @@ const Router = require("express").Router();
 const ProjectRouter = require("./projects");
 const ConstitutionRouter = require("./constitution/constitution");
 const ObjectiveRouter = require("./constitution/objective");
+const PhasesRouter = require("./constitution/phases");
 
 
 Router.use('/projects', ProjectRouter);
 
 Router.use('/constitutions' ,ConstitutionRouter);
 Router.use('/objectives' ,ObjectiveRouter);
+Router.use('/phases' ,PhasesRouter);
 
 module.exports = Router;
