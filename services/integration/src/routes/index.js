db91325c31459cb41af4cc4e75d8a4450378cec7
RicardoJ <rjdiazzh@gmail.com>
2019-03-18 22:52:31 -0500

    [INTG][OTHER REQUIREMENTS] feature-otherRequirements adding controller , model and routes

--- services/integration/src/routes/index.js
+++ services/integration/src/routes/index.js
@@ -4,13 +4,13 @@ const ConstitutionRouter = require("./constitution/constitution");
 const ObjectiveRouter = require("./constitution/objective");
 const PhasesRouter = require("./constitution/phases");
 const HitoRouter = require("./constitution/hito");
-
+const OtherRequirementsRouter = require("./constitution/otherRequirements");
 
 Router.use('/projects', ProjectRouter);
-
 Router.use('/constitutions' ,ConstitutionRouter);
 Router.use('/objectives' ,ObjectiveRouter);
 Router.use('/phases' ,PhasesRouter);
 Router.use('/hitos' ,HitoRouter);
+Router.use('/otherRequirements' ,OtherRequirementsRouter);
 
 module.exports = Router;
