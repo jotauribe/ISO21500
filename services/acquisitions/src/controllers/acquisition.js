f894ba0907fd0e24d3182aa0fff8f181f70830e5
RicardoJ <rjdiazzh@gmail.com>
2019-03-19 14:42:32 -0500

    [ACQ][ACQUISITIONS] feature-acquisitions adding routes , controller ,and  model

---
+++ services/acquisitions/src/controllers/acquisition.js
@@ -0,0 +1,38 @@
+const Acquisition = require('../models/acquisition');
+const { asyncHandler } = require('../utils');
+
+const create = asyncHandler(async (req, res) => {
+  const acquisition = new Acquisition(req.body);
+  await acquisition.save();
+  res.send({ message:'Acquisition Created successfully'});
+});
+
+const get = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const acquisition = await Acquisition.findById(id);
+  res.send({ message: acquisition });
+});
+
+const find = asyncHandler(async (req, res) => {
+  const acquisition = await Acquisition.find({});
+
+  res.send({ message: acquisition });
+});
+
+const update = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const updatedAcquisition = await Acquisition.findByIdAndUpdate(id, {
+    $set: req.body
+  });
+
+  res.send({ message: 'Acquisition udpated', updatedAcquisition });
+});
+
+const remove = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  await Acquisition.findByIdAndRemove(id);
+
+  res.send({ message: 'Deleted successfully!' });
+});
+
+module.exports = { create, get, find, remove, update };
--- services/acquisitions/src/routes/index.js
+++ services/acquisitions/src/routes/index.js
@@ -1,10 +1,10 @@
 const Router = require("express").Router();
 const ProviderRouter = require("./provider");
-//const AcquisitionsRouter = require("./acquisition");
+const AcquisitionsRouter = require("./acquisition");
 
 
 Router.use('/providers', ProviderRouter);
-//Router.use('/acquisitions' ,AcquisitionsRouter);
+Router.use('/acquisitions' ,AcquisitionsRouter);
 
 
 module.exports = Router;
