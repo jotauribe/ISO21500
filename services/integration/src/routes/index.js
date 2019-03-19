d8b63321cbbc5a320642dfd5246ae10277d68b03
RicardoJ <rjdiazzh@gmail.com>
2019-03-18 22:21:39 -0500

    [INTG][HITO] feature-hito add route , controller and model of hito

--- services/integration/src/routes/index.js
+++ services/integration/src/routes/index.js
@@ -3,6 +3,7 @@ const ProjectRouter = require("./projects");
 const ConstitutionRouter = require("./constitution/constitution");
 const ObjectiveRouter = require("./constitution/objective");
 const PhasesRouter = require("./constitution/phases");
+const HitoRouter = require("./constitution/hito");
 
 
 Router.use('/projects', ProjectRouter);
@@ -10,5 +11,6 @@ Router.use('/projects', ProjectRouter);
 Router.use('/constitutions' ,ConstitutionRouter);
 Router.use('/objectives' ,ObjectiveRouter);
 Router.use('/phases' ,PhasesRouter);
+Router.use('/hitos' ,HitoRouter);
 
 module.exports = Router;
