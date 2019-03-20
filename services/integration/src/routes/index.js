3acfa82f1ec46860a46dc24d4c3198d13a2d0576
RicardoJ <rjdiazzh@gmail.com>
2019-03-19 22:43:59 -0500

    [INTG][LESSONS] feature-lesson adding route , controller and model

--- services/integration/src/routes/index.js
+++ services/integration/src/routes/index.js
@@ -7,6 +7,7 @@ const HitoRouter = require('./constitution/hito');
 const OtherRequirementsRouter = require('./constitution/otherRequirements');
 const limitationsRouter = require('./constitution/limitations');
 const AuthorityLevelRouter = require('./constitution/authorityLevel');
+const LessonRouter = require('./lesson');
 
 Router.use('/projects', ProjectRouter);
 Router.use('/constitutions' ,ConstitutionRouter);
@@ -16,5 +17,6 @@ Router.use('/hitos' ,HitoRouter);
 Router.use('/otherRequirements' ,OtherRequirementsRouter);
 Router.use('/limitations' ,limitationsRouter);
 Router.use('/authorityLevel' ,AuthorityLevelRouter);
+Router.use('/lessons' ,LessonRouter);
 
 module.exports = Router;
