e78d5be236b997bab37acd38b039af6e0dbd437a
RicardoJ <rjdiazzh@gmail.com>
2019-03-19 20:45:47 -0500

    [INTG][PHASES]- feature-phases change quotes in index and send in controller

--- services/integration/src/controllers/constitution/phases.js
+++ services/integration/src/controllers/constitution/phases.js
@@ -10,13 +10,13 @@ const create = asyncHandler(async (req, res) => {
 const get = asyncHandler(async (req, res) => {
   const { id } = req.params;
   const phases = await Phases.findById(id);
-  res.send({ message: phases });
+  res.send( phases );
 });
 
 const find = asyncHandler(async (req, res) => {
   const phases = await Phases.find({});
 
-  res.send({ message: phases });
+  res.send(phases );
 });
 
 const update = asyncHandler(async (req, res) => {
--- services/integration/src/routes/index.js
+++ services/integration/src/routes/index.js
@@ -1,8 +1,8 @@
-const Router = require("express").Router();
-const ProjectRouter = require("./projects");
-const ConstitutionRouter = require("./constitution/constitution");
-const ObjectiveRouter = require("./constitution/objective");
-const PhasesRouter = require("./constitution/phases");
+const Router = require('express').Router();
+const ProjectRouter = require('./projects');
+const ConstitutionRouter = require('./constitution/constitution');
+const ObjectiveRouter = require('./constitution/objective');
+const PhasesRouter = require('./constitution/phases');
 
 
 Router.use('/projects', ProjectRouter);
