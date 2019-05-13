122d91d1a499daedfa9a5c51b257da39f9bd92ca
JotaUribe <urib304@gmail.com>
2019-05-13 01:54:37 -0500

    [WEBSITE] - Changing sidebar icons

--- website/src/app/core/components/sidebar/sidebar.component.ts
+++ website/src/app/core/components/sidebar/sidebar.component.ts
@@ -33,24 +33,8 @@ export class SidebarComponent implements OnInit {
     private matIconRegistry: MatIconRegistry,
     private sanitizer: DomSanitizer
   ) {
-    this.matIconRegistry.addSvgIcon(
-      'gpt_integration',
-      this.sanitizer.bypassSecurityTrustResourceUrl(
-        'assets/icons/integration.svg'
-      )
-    );
-
-    this.matIconRegistry.addSvgIcon(
-      'gpt_resources',
-      this.sanitizer.bypassSecurityTrustResourceUrl(
-        'assets/icons/resources.svg'
-      )
-    );
-    this.matIconRegistry.addSvgIcon(
-      'gpt_acquisitions',
-      this.sanitizer.bypassSecurityTrustResourceUrl(
-        'assets/icons/acquisitions.svg'
-      )
+    this.matIconRegistry.addSvgIconSet(
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/icons/features.svg')
     );
   }
 
