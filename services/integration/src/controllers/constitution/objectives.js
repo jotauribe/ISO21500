0177231653a35e4f13b33473ebeee90163691330
RicardoJ <rjdiazzh@gmail.com>
2019-06-03 14:15:49 -0500

    [SERVICES] - [INTEGRATION] - objectives: adding routes mdel , and controller of objectives

---
+++ services/integration/src/controllers/constitution/objectives.js
@@ -0,0 +1,32 @@
+const Objective = require('../../models/constitution/objectives.model');
+const { asyncHandler } = require('../../utils');
+
+const create = asyncHandler(async (req, res) => {
+  const objectives = req.body;
+  const { projectId } = req;
+
+  Objective.insertMany(objectives.map(o => ({ ...o, projectId })));
+
+  res.send({ message: 'Objectives Created successfully' });
+});
+
+const find = asyncHandler(async (req, res) => {
+  const objective = await Objective.find({});
+
+  res.send(objective);
+});
+
+const update = asyncHandler(async (req, res) => {
+  const { projectId } = req;
+  const { id } = req.params;
+  const updatedDocument = await Objective.findOneAndUpdate(
+    { projectId, _id: id },
+    {
+      $set: req.body
+    }
+  );
+
+  res.send({ message: 'udpated.', updatedDocument });
+});
+
+module.exports = { create, find, update };
