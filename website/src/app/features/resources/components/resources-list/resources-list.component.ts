63940a11146bb1deb1058d400839e3ded2b2ad8b
JotaUribe <yosoyuribe@gmail.com>
2019-06-23 16:15:58 -0500

    [WEBSITE] - [RESOURCES] - Resources: Form Schema

--- website/src/app/features/resources/components/resources-list/resources-list.component.ts
+++ website/src/app/features/resources/components/resources-list/resources-list.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { Store, select } from '@ngrx/store';
+import { CoreState } from '~/app/core/store';
+import { startWith } from 'rxjs/operators';
+import {
+  UpdateResources,
+  LoadResources
+} from '~/app/core/store/resources/resources.actions';
 
 @Component({
   selector: 'gpt-resources-list',
@@ -6,10 +15,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resources-list.component.scss']
 })
 export class ResourcesListComponent implements OnInit {
+  schema = {
+    sections: [
+      {
+        name: 'resources',
+        title: 'Recursos',
+        dataPath: 'resources',
+        isList: true,
+        schema: {
+          title: 'name',
+          prefix: 'kind',
+          secondaryInfo: [
+            { title: 'Comentarios', info: 'comments' },
+            { title: 'Cantidad', info: 'quantity' }
+          ]
+        },
+        fields: [
+          { name: 'name', value: '', placeholder: 'Nombre Recurso' },
+          { name: 'kind', value: '', placeholder: 'Tipo' },
+          {
+            name: 'comments',
+            value: '',
+            placeholder: 'Comentarios'
+          },
+          {
+            name: 'quantity',
+            value: '',
+            placeholder: 'Cantidad'
+          }
+        ]
+      }
+    ]
+  };
 
-  constructor() { }
+  data: Observable<any>;
 
-  ngOnInit() {
+  isDataLoaded: Observable<boolean>;
+
+  constructor(private store: Store<CoreState>, private route: ActivatedRoute) {
+    this.store.dispatch(new LoadResources(this.getProjectId()));
+    this.data = this.store.pipe(select(s => s.resources.data));
+    this.isDataLoaded = this.store.pipe(
+      select(s => s.resources.isLoaded),
+      startWith(false)
+    );
   }
 
+  ngOnInit() {
+    console.log('HOLA BEBE');
+  }
+
+  getProjectId() {
+    return this.route.snapshot.paramMap.get('projectId');
+  }
+
+  pushChanges(data) {
+    const projectId = this.getProjectId();
+    this.store.dispatch(new UpdateResources({ resources: data, projectId }));
+  }
 }
